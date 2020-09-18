761576c8c9e25d596091503b8ada8e2c91f306f5
kumaGoro <todira.island@gmail.com>
2020-09-18 15:48:01 +0900

    modify methods #1

--- src/servlet/LoginServlet.java
+++ src/servlet/LoginServlet.java
@@ -8,19 +8,45 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import model.Login;
+import model.LoginLogic;
 
 @WebServlet("/LoginServlet")
 public class LoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//フォワード
 		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/login.jsp");
 		dispatcher.forward(request, response);
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/loginSuccess.jsp");
-		dispatcher.forward(request, response);
+		//リクエストパラメータの取得
+		request.setCharacterEncoding("UTF-8");
+		String name = request.getParameter("name");
+		String pass = request.getParameter("pass");
+		//ログイン処理の実行
+		Login login = new Login(name,pass);
+		LoginLogic bo = new LoginLogic();
+		boolean result = bo.execute(login);
+
+		//ログイン処理の成否によって処理を分岐
+		if(result) { //ログイン成功時
+			//セッションスコープにユーザーIDを保存
+			HttpSession session = request.getSession();
+			session.setAttribute("name", name);
+
+			//フォワード
+			RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/loginSuccess.jsp");
+			dispatcher.forward(request, response);
+		}else { //ログイン失敗時
+			//リダイレクト
+			response.sendRedirect("/testJavaApp/LoginServlet");
+		}
+
 	}
 
 }
